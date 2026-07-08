10b55b90ef3077999e0f04ec437c387af0a5d5fe
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-07-08 15:48:40 +0500

    HomeCards Added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,33 @@
+import MeetingTypeList from '@/components/shared/MeetingTypeList'
 import React from 'react'
 
 const Home = () => {
   return (
     <section className="flex flex-col size-full gap-10 text-white">
 
-      <h1 className="text-3xl font-bold">
-        Home
-      </h1>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        
+        <div className="flex flex-col h-full
+        justify-between max-md:px-5 max-md:py-8
+        lg:p-11">
 
-      
+          <h2 className="glassmorphism max-w-[270px] rounded
+          py-2 text-center text-base font-normal">Upcoming meeting at: 12:30 PM</h2>
+
+          <div className="flex flex-col gap-2">
+
+            <h1 className="text-4xl font-extrabold lg:text-7xl">11:30 AM</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">Saturday, March 23, 2024</p>
+            
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <MeetingTypeList />
+
     </section>
   )
 }
